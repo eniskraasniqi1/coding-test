aa5451c4d74e22b464aadda7847e4a77d7c9021b
Enis <enisk@starlabspro.com>
2022-03-19 13:35:27 +0100

    Add Custom Select component

---
+++ src/components/atoms/select-field/index.tsx
@@ -0,0 +1,39 @@
+import { Input, SelectOption } from "src/types";
+
+import styles from "./select-field.module.scss";
+
+interface SelectFieldProps {
+  label: string;
+  error: string | undefined;
+  select: Input;
+  required?: boolean;
+  options?: SelectOption[];
+}
+
+const SelectField = ({
+  label,
+  error,
+  select,
+  required,
+  options,
+}: SelectFieldProps) => {
+  return (
+    <>
+      <div className={styles.selectWrapper}>
+        <label htmlFor={select.id}>
+          {label} {required && <span>*</span>}
+        </label>
+        <select {...select}>
+          {options?.map((singleOption) => (
+            <option key={singleOption.value} value={singleOption.value}>
+              {singleOption.label}
+            </option>
+          ))}
+        </select>
+        {error && <div>{error}</div>}
+      </div>
+    </>
+  );
+};
+
+export default SelectField;
